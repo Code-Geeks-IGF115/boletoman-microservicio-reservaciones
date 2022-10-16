ef4ec701f76b9ee94e86f8217e912e0c84024e25
HB21009 <hb21009@ues.edu.sv>
2022-10-16 05:25:48 +0000

    Controlador CategoriaButaca

---
+++ src/Form/CategoriaButacaType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\CategoriaButaca;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoriaButacaType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('codigo')
+            ->add('precioUnitario')
+            ->add('nombre')
+            ->add('salaDeEventos')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => CategoriaButaca::class,
+        ]);
+    }
+}
