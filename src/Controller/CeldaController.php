a142a84e0bfb0beb2b5032c4e508d9825282cd28
Cargueta01 <aa20099@ues.edu.sv>
2022-10-18 02:01:48 +0000

    funcion celdas categoria en proceso

--- src/Controller/CeldaController.php
+++ src/Controller/CeldaController.php
@@ -14,6 +14,8 @@ use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 use App\Repository\CategoriaButacaRepository;
 use App\Service\ResponseHelper;
+use Exception;
+use Nelmio\CorsBundle;
 
 #[Route('/celda')]
 class CeldaController extends AbstractController
@@ -66,31 +68,74 @@ class CeldaController extends AbstractController
     CategoriaButacaRepository $categoriaButacaRepository,
     CeldaRepository $celdaRepository,
     $idCategoria): JsonResponse
-    {
+    { 
+         //String $result = "";
         //recuperando la categoria butaca
-        $categoriaButaca = $categoriaButacaRepository->find($idCategoria);
+        $categoriaButaca = $categoriaButacaRepository->find($idCategoria);        
+        if ($categoriaButaca == null) {//verifica si el id ingresado existe 
+            return $this->responseHelper->responseMessage("id no existe");
+        }
         $salaDeEvento=$categoriaButaca->getSalaDeEventos();
+
         //recuperar todas las celdas de esta sala de eventos
         $celdas=$celdaRepository->findBy(['categoriaButaca'=>$categoriaButaca]);
-        //crear celdas
-        for ($fila=1; $fila <= $salaDeEvento->getFilas() ; $fila++) { 
-            for ($columna=1; $columna <= $salaDeEvento->getColumnas() ; $columna++) { 
-                //recorrer las celdas del request y las celdas de la base de datos en
-                //simultaneo y comparar los atributos de fila y columna
-                // y si son iguales entonces actualizar(update) 
-                //la celda (cantidad butacas y asignar categoriaButaca)y 
-                // guardarla en la base de datos
 
-                // $celda = new Celda();
-                // $celda->getFila($fila);
-                // $celda->getColumna($columna);
-                // $celda->setSalaDeEventos($salaDeEvento);
-                // $celdaRepository->save($celda,true);
-            }
-        }
-        // $celdaRepository->save($celda, true);
-       
-        return $this->responseHelper->responseMessage("celdas guardadas");
+        $categoriaId=$request->query->get('celdas', null); 
+        var_dump($celdas);
+        // //almacenando json request en array para comparar
+        // if ($request->getContent()) {
+        //     $parametrosarray = json_decode($request->getContent(), true); 
+        // }
+
+        // //crear celdas
+        // for ($fila=1; $fila <= $salaDeEvento->getFilas() ; $fila++) { 
+        //     for ($columna=1; $columna <= $salaDeEvento->getColumnas() ; $columna++) { 
+        //         //recorrer las celdas del request y las celdas de la base de datos en
+        //         //simultaneo y comparar los atributos de fila y columna
+        //         // y si son iguales entonces actualizar(update) 
+        //         //la celda (cantidad butacas y asignar categoriaButaca)y 
+        //         // guardarla en la base de datos
+
+        //         $celda = new Celda();
+        //         //$celda->setFila(3+$columna-1);
+        //         //$celda->setColumna(1+$columna-1);
+        //         if (($parametrosarray["celdas"][0]["fila"] == $celdas[$fila-1]->getFila()) && 
+        //         ($parametrosarray["celdas"][0]["columna"] == $celdas[$columna-1]->getColumna())){
+        //             $celda->setSalaDeEventos($salaDeEvento);
+        //             $celda->setCantidadButacas($fila);
+        //             $celda->setCategoriaButaca($categoriaButaca);
+        //             $celdaRepository->save($celda,true);
+                    
+        //             /*if ($fila-1 == $salaDeEvento->getFilas() &&  $columna-1 == $salaDeEvento->getColumnas()) {
+        //                 $result = "se guardo el primer if fila: ".$fila." columna: ".$columna;
+        //                 return $this->responseHelper->responseMessage($result);
+        //             }    */  
+        //         }
+        //         elseif (($parametrosarray["celdas"][1]["fila"] == $celdas[$fila-1]->getFila()) && 
+        //         ($parametrosarray["celdas"][1]["columna"] == $celdas[$columna-1]->getColumna())) {
+        //             $celda->setSalaDeEventos($salaDeEvento);
+        //             $celda->setCantidadButacas($fila);
+        //             $celda->setCategoriaButaca($categoriaButaca);
+        //             $celdaRepository->save($celda,true);
+        //             $result = "se guardo la segunda fila: ". $fila . " columna: " . $columna;
+        //             /*if ($fila-1 == $salaDeEvento->getFilas() && $columna-1 == $salaDeEvento->getColumnas()) {
+        //                 $result = "se guardo el segundo if fila: ". $fila . " columna: " . $columna;
+        //                 return $this->responseHelper->responseMessage($result);
+        //             }  */                    
+        //         }
+                
+        //         // $celda->getFila($fila);
+        //         // $celda->getColumna($columna);
+        //         // $celda->setSalaDeEventos($salaDeEvento);
+        //         // $celdaRepository->save($celda,true);
+        //     }
+        // }
+        /*$array= array();
+        for ($i=0; $i < count($celdas); $i++) { 
+            $array[] = $celdas[$i]->getCantidadButacas();
+
+        }*/
+        return $this->responseHelper->responseMessage($celdas);
     }
 
 
