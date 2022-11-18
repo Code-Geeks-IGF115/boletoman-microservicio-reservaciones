d3e73b34fb3ecb2ab5ba0d12900b8f0f1e413730
Angel273 <mauricioangel2736@gmail.com>
2022-11-18 17:10:06 +0000

    Funcionalidad validar compras

--- src/Controller/DisponibilidadController.php
+++ src/Controller/DisponibilidadController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity\Disponibilidad;
+use Symfony\Component\HttpFoundation\{Response,JsonResponse};
 use App\Repository\DisponibilidadRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -9,78 +10,80 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Service\ResponseHelper;
 use Symfony\Component\Serializer\Encoder\JsonEncode;
 
-#[Route('/disponibilidad')]
-class DisponibilidadController extends AbstractController
-{
+
+
+ #[Route('/disponibilidad')]
+ class DisponibilidadController extends AbstractController
+ {
     private ResponseHelper $responseHelper;
 
-
+    
     public function __construct(ResponseHelper $responseHelper)
     {
         $this->responseHelper = $responseHelper;
     }
-    // #[Route('/', name: 'app_disponibilidad_index', methods: ['GET'])]
-    // public function index(DisponibilidadRepository $disponibilidadRepository): Response
-    // {
-    //     return $this->render('disponibilidad/index.html.twig', [
-    //         'disponibilidads' => $disponibilidadRepository->findAll(),
-    //     ]);
-    // }
+     #[Route('/', name: 'app_disponibilidad_index', methods: ['GET'])]
+      public function index(DisponibilidadRepository $disponibilidadRepository): Response
+      {
+          return $this->render('disponibilidad/index.html.twig', [
+              'disponibilidads' => $disponibilidadRepository->findAll(),
+          ]);
+      }
 
-    // #[Route('/new', name: 'app_disponibilidad_new', methods: ['GET', 'POST'])]
-    // public function new(Request $request, DisponibilidadRepository $disponibilidadRepository): Response
-    // {
-    //     $disponibilidad = new Disponibilidad();
-    //     $form = $this->createForm(DisponibilidadType::class, $disponibilidad);
-    //     $form->handleRequest($request);
+//      #[Route('/new', name: 'app_disponibilidad_new', methods: ['GET', 'POST'])]
+//      public function new(Request $request, DisponibilidadRepository $disponibilidadRepository): Response
+//      {
+//          $disponibilidad = new Disponibilidad();
+//          $form = $this->createForm(DisponibilidadType::class, $disponibilidad);
+//          $form->handleRequest($request);
 
-    //     if ($form->isSubmitted() && $form->isValid()) {
-    //         $disponibilidadRepository->save($disponibilidad, true);
+//          if ($form->isSubmitted() && $form->isValid()) {
+//              $disponibilidadRepository->save($disponibilidad, true);
 
-    //         return $this->redirectToRoute('app_disponibilidad_index', [], Response::HTTP_SEE_OTHER);
-    //     }
+//              return $this->redirectToRoute('app_disponibilidad_index', [], Response::HTTP_SEE_OTHER);
+//          }
 
-    //     return $this->renderForm('disponibilidad/new.html.twig', [
-    //         'disponibilidad' => $disponibilidad,
-    //         'form' => $form,
-    //     ]);
-    // }
+//          return $this->renderForm('disponibilidad/new.html.twig', [
+//              'disponibilidad' => $disponibilidad,
+//              'form' => $form,
+//          ]);
+//      }
 
-    // #[Route('/{id}', name: 'app_disponibilidad_show', methods: ['GET'])]
-    // public function show(Disponibilidad $disponibilidad): Response
-    // {
-    //     return $this->render('disponibilidad/show.html.twig', [
-    //         'disponibilidad' => $disponibilidad,
-    //     ]);
-    // }
+//      #[Route('/{id}', name: 'app_disponibilidad_show', methods: ['GET'])]
+//      public function show(Disponibilidad $disponibilidad): Response
+//      {
+//          return $this->render('disponibilidad/show.html.twig', [
+//              'disponibilidad' => $disponibilidad,
+//          ]);
+//      }
 
-    // #[Route('/{id}/edit', name: 'app_disponibilidad_edit', methods: ['GET', 'POST'])]
-    // public function edit(Request $request, Disponibilidad $disponibilidad, DisponibilidadRepository $disponibilidadRepository): Response
-    // {
-    //     $form = $this->createForm(DisponibilidadType::class, $disponibilidad);
-    //     $form->handleRequest($request);
+//      #[Route('/{id}/edit', name: 'app_disponibilidad_edit', methods: ['GET', 'POST'])]
+//      public function edit(Request $request, Disponibilidad $disponibilidad, DisponibilidadRepository $disponibilidadRepository): Response
+//      {
+//          $form = $this->createForm(DisponibilidadType::class, $disponibilidad);
+//          $form->handleRequest($request);
 
-    //     if ($form->isSubmitted() && $form->isValid()) {
-    //         $disponibilidadRepository->save($disponibilidad, true);
+//          if ($form->isSubmitted() && $form->isValid()) {
+//              $disponibilidadRepository->save($disponibilidad, true);
 
-    //         return $this->redirectToRoute('app_disponibilidad_index', [], Response::HTTP_SEE_OTHER);
-    //     }
+//              return $this->redirectToRoute('app_disponibilidad_index', [], Response::HTTP_SEE_OTHER);
+//          }
 
-    //     return $this->renderForm('disponibilidad/edit.html.twig', [
-    //         'disponibilidad' => $disponibilidad,
-    //         'form' => $form,
-    //     ]);
-    // }
+//          return $this->renderForm('disponibilidad/edit.html.twig', [
+//              'disponibilidad' => $disponibilidad,
+//              'form' => $form,
+//          ]);
+//      }
 
-    // #[Route('/{id}', name: 'app_disponibilidad_delete', methods: ['POST'])]
-    // public function delete(Request $request, Disponibilidad $disponibilidad, DisponibilidadRepository $disponibilidadRepository): Response
-    // {
-    //     if ($this->isCsrfTokenValid('delete'.$disponibilidad->getId(), $request->request->get('_token'))) {
-    //         $disponibilidadRepository->remove($disponibilidad, true);
-    //     }
+//      #[Route('/{id}', name: 'app_disponibilidad_delete', methods: ['POST'])]
+//      public function delete(Request $request, Disponibilidad $disponibilidad, DisponibilidadRepository $disponibilidadRepository): Response
+//      {
+//          if ($this->isCsrfTokenValid('delete'.$disponibilidad->getId(), $request->request->get('_token'))) {
+//              $disponibilidadRepository->remove($disponibilidad, true);
+//          }
 
-    //     return $this->redirectToRoute('app_disponibilidad_index', [], Response::HTTP_SEE_OTHER);
-    // }
+//          return $this->redirectToRoute('app_disponibilidad_index', [], Response::HTTP_SEE_OTHER);
+//      }
 
     #[Route('/bloquearbutacas', name: 'app_disponibilidad_bloquear_butacas',  methods: ['POST'])]
     public function bloquearbutacas(Request $request, DisponibilidadRepository $disponibilidadRepository): JsonResponse
@@ -104,7 +107,7 @@ class DisponibilidadController extends AbstractController
         if($cantidadButacasBuscadas==$cantidadButacasBloqueadas){
             $mensaje='Butacas modificadas con éxito';
         }else{
-            $mensaje='Algunas butacas ya han sido compradas';
+            $mensaje='Algunas butacas no han sido bloqueadas';
         }
         //agregar array ids de butacas modificadas a la data
         $data=[
@@ -140,7 +143,7 @@ class DisponibilidadController extends AbstractController
         if($cantidadButacasBuscadas==$cantidadButacasDesbloqueadas){
             $mensaje='Butacas modificadas con éxito';
         }else{
-            $mensaje='Algunas butacas continuan bloqueadas';
+            $mensaje='Algunas butacas no fueron desbloqueadas';
         }
         //agregar array ids de butacas modificadas a la data
         $data=[
@@ -153,40 +156,74 @@ class DisponibilidadController extends AbstractController
      }
 
      #[Route('/comprarbutacas', name: 'app_disponibilidad_comprar_butacas', methods: ['POST'])]
-     public function comprarbutacas(Request $request, DisponibilidadRepository $disponibilidadRepository): JsonResponse
+     public function comprarbutacas(Request $request, DisponibilidadRepository $disponibilidadRepository): Response
      {
+        $responsegud = new Response(
+            'Fallo',
+            Response::HTTP_OK,
+            array('content-type' => 'text/html')
+        );
+        $responsebad = new Response(
+            'bien',
+            Response::HTTP_PRECONDITION_FAILED,
+            array('content-type' => 'text/html')
+        );
+       
+        //Definir el estado habilitado para comprar
         $estado="Bloqueado";
+
+        //convertir el request en array
         $parametros = $request->toArray();
-        $butacasIDs=$parametros["butacas"];
-        
-        
+
+        //Obtener los arreglos de disponibilidades
+        $disp=$parametros["disponibilidades"];
+
+        //Obtener el numero de arreglos de butacas
+        foreach($disp as $diponibilidad)
+        {
+            $butacas=$diponibilidad["butacas"];
+
+            //Obtener el butaca_id de todos los arreglos butacas del request
+            foreach($butacas as $butaca)
+            {
+                $butacasIDs[]=$butaca;
+            }
+
+        }
+               
         // trae todas las disponibilidades donde el id del evento, estado disponible e id butaca corresponden
         $disponibilidadesButaca=$disponibilidadRepository->findByEstado($parametros["idEvento"],$estado, $butacasIDs);
-        foreach ($disponibilidadesButaca as $key => $disponibilidadButaca){
-                $disponibilidadButaca->setDisponible('No disponible');
-                $disponibilidadRepository->save($disponibilidadButaca, true);
-                // agregar aqui id butaca al array 
-        }
-        //calcular métricas
-        
+
+
+        //calcular métricas 
         $cantidadButacasCompradas=count($disponibilidadesButaca);
         $cantidadButacasBuscadas=count($butacasIDs);
-        $cantidadButacasModificadas=$cantidadButacasBuscadas-$cantidadButacasCompradas;
-        $mensaje=null;
+
         //id de butacas modificadas
         
         if($cantidadButacasBuscadas==$cantidadButacasCompradas){
-            $mensaje='Butacas compradas con éxito';
+            //Modifica la disponible a todas las disponibilidades que corresponden
+            foreach ($disponibilidadesButaca as $key => $disponibilidadButaca)
+            {
+                $disponibilidadButaca->setDisponible('No disponible');
+                $disponibilidadRepository->save($disponibilidadButaca, true);
+                // agregar aqui id butaca al array 
+                return $responsegud;
+            }
+        return $this->redirectToRoute('app_disponibilidad_index', [], Response::HTTP_OK);
         }else{
-            $mensaje='Algunas butacas no fueron compradas';
+            return $responsebad;
         }
-        //agregar array ids de butacas modificadas a la data
-        $data=[
-            'buscadas'=>$cantidadButacasBuscadas,
-            'compradas'=>$cantidadButacasCompradas,
-            'no-validas'=>$cantidadButacasModificadas,
-            'mensaje'=>$mensaje
-        ];
-        return $this->responseHelper->responseDatos($data);
      }
+
+     #[Route('/butacasVendidas', name: 'app_disponibilidad_comprar_butacas', methods: ['POST'])]
+     public function butacasVendidas(Request $request, DisponibilidadRepository $disponibilidadRepository): Response{
+        $responsebad = new Response(
+            'Fallo',
+            Response::HTTP_OK,
+            array('content-type' => 'text/html')
+        );
+        return $responsebad;;
+     }
+     
 }
