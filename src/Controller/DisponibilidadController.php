1c9ffbb9cf97797ccf0a2956a187fcff0759a919
cargueta01 <aa20099@ues.edu.sv>
2022-11-21 04:01:04 +0000

    disponibilidad cliente v13

--- src/Controller/DisponibilidadController.php
+++ src/Controller/DisponibilidadController.php
@@ -277,7 +277,7 @@ use Symfony\Component\Serializer\Encoder\JsonEncode;
     {
         $mensaje="Hola Mundo!";
         $parametrosDetalleCompra = $request->toArray();
-        //var_dump($parametrosDetalleCompra);
+        dd($parametrosDetalleCompra);
         
         /*try{
             // recibiendo parametros
